1de915fc87eb420b81ceb328320782e21402556d
AspieArchitect <AspieArchitect@gmail.com>
2020-06-28 18:43:55 +0100

    When applied this caters for basic markdown tables but there are edge cases that still break markdown

--- ScriptHelpers/MarkDownList.ps1
+++ ScriptHelpers/MarkDownList.ps1
@@ -6,55 +6,52 @@ $fileContentLines=$fileContent.Split([Environment]::NewLine)
 $tableColumnCount=0
 $currentColumnCount=0
 $concatenatedHTMLList=""
-Clear-Host
+$listOpened = $True
+#Clear-Host
 
 ForEach ($line in $fileContentLines){
 
     # Signifies the start of a markdown table
     If (($line.StartsWith("| Key |")) -or ($line.StartsWith("| Name |"))){
         $tableColumnCount=($line.Split("|")).count
+        Write-Output $line
+        continue
     }
 
+    # Empty line in an existing table means time to close the table
     if(($line.Length -eq 0) -and ($tableColumnCount -gt 0)){
         $tableColumnCount=0
         $currentColumnCount=0
         $concatenatedHTMLList=""
     }
-    if($tableColumnCount -eq 0){
+
+    if($tableColumnCount -gt 0){
+        $lineArray=$line.Split("|")
+        $currentColumnCount=($lineArray).count    
+    }
+    # If you are not in a table or the table line contains the same number of columns
+    # As the title line then just print it out.
+    if(($currentColumnCount -eq $tableColumnCount) -or ($tableColumnCount -eq 0)){
         Write-Output $line
+        continue
+    }
+
+        
+    if($line.Trim().StartsWith("*")){
+        $lastItem=$lineArray[0]
+        $newItem="{0}</li>" -f $lastItem.Replace("* ","<li>")
+        if ($line.EndsWith("|")){
+            $concatenatedHTMLList += "{0}</ul>|" -f $newItem.Replace("_ </li>","</li>")
+            Write-output $concatenatedHTMLList
+        } else{
+            $concatenatedHTMLList +=  $newItem
+        }
+
     }
     else{
-        $lineArray=$line.Split("|")
-        $currentColumnCount=($lineArray).count
-        if($line.StartsWith("*")){
-            $lastItem=$lineArray[0]
-            }
-        else{
-            $lastItem=$lineArray[$currentColumnCount - 1]
-            $lineStart=$lineArray[0..($lineArray.Length -2)] -join ""
-            
-        }
-        if($currentColumnCount -eq $tableColumnCount){
-            Write-Output $line
-        }
-        else{
-            if(($tableColumnCount -1) -eq $currentColumnCount){
-                $newItem="{0}</li>" -f $lastItem.Replace("* ","<ul><li>")
-                $concatenatedHTMLList = $newItem
-            }
-            else{
-                $newItem="{0}</li>" -f $lastItem.Replace("* ","<li>")
-            }
-            if($lastItem.Length -gt 0){
-                $line=$line.Replace($lastItem,$newItem).Replace("</li>|","</li></ul>|")
-                $concatenatedHTMLList+=$line
-            }
-            if($concatenatedHTMLList.EndsWith("</li></ul>|")){
-                $concatenatedHTMLList = $lineStart + $concatenatedHTMLList
-                Write-Output $concatenatedHTMLList
-            }
-                
+        $lastItem=$lineArray[$currentColumnCount - 1]
+        $lineStart=$lineArray[0..($lineArray.Length -2)] -join "|"
+        $concatenatedHTMLList="{0}{1}</li>" -f $linestart,$lastItem.Replace("_* ","|<ul><li>")
 
-        }
     }
 }
