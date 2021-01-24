db7dd830f9974903fc0db64ee5916df2d20dd817
AspieArchitect <AspieArchitect@gmail.com>
2021-01-24 09:35:57 +0000

    When applied this shifts the SQLDoc project and output into folders not in the user profile

--- RunSQLDoc.ps1
+++ RunSQLDoc.ps1
@@ -1,17 +1,24 @@
 Set-Alias sqldoc 'C:\Program Files (x86)\Red Gate\SQL Doc 5\SQLDoc.exe' -Scope Script
 
-# base_path is the folder containing this script
-$base_path = Split-Path -Parent $PSCommandPath
-$documentation_path = "$base_path\SQLDoc\DADSPCJUNE2014_documentation"
+# Location where SQLDoc projects are held
+$SQLDoc_Project= "C:\code\SQLDoc\Projects\RedgateDataCatalog.sqldoc"
+
+# Location where RedGate SQLDoc will be output
+$SQLDoc_Path = "C:\Documentation\SQLDoc\DADSPCJUNE2014_documentation"
+
+# Create the output location if it doesn't already exist
+if(!(Test-Path -Path $SQLDoc_Path)){
+    New-Item -ItemType directory -Path $SQLDoc_Path
+}
 
 # Make sure we start with a clean folder
-Get-ChildItem -Path $documentation_path -Recurse|Remove-Item -Force -Recurse
+Get-ChildItem -Path $SQLDoc_Path -Recurse|Remove-Item -Force -Recurse
 
 # Generate both md and html documentation
-sqldoc /server:DADSPCJUNE2014 /database:Redgate_SqlDataCatalog  /project:RedgateDataCatalog.sqldoc /fileType:md
-sqldoc /server:DADSPCJUNE2014 /database:Redgate_SqlDataCatalog  /project:RedgateDataCatalog.sqldoc /fileType:html
+sqldoc /server:DADSPCJUNE2014 /database:Redgate_SqlDataCatalog  /project:$SQLDoc_Project /outputfolder:$SQLDoc_Path /fileType:md
+sqldoc /server:DADSPCJUNE2014 /database:Redgate_SqlDataCatalog  /project:$SQLDoc_Project /outputfolder:$SQLDoc_Path /fileType:html
 
-& ".\ScriptHelpers\index_to_readme.ps1" "$documentation_path"
-& ".\ScriptHelpers\md_to_html_link.ps1" "$documentation_path"
-& ".\ScriptHelpers\md_to_bullet_list.ps1" "$documentation_path"
-& ".\ScriptHelpers\object_to_readme.ps1" "$documentation_path"
+& ".\ScriptHelpers\index_to_readme.ps1" "$SQLDoc_Path"
+& ".\ScriptHelpers\md_to_html_link.ps1" "$SQLDoc_Path"
+& ".\ScriptHelpers\md_to_bullet_list.ps1" "$SQLDoc_Path"
+& ".\ScriptHelpers\object_to_readme.ps1" "$SQLDoc_Path"
